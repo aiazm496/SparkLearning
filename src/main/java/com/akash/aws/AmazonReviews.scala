61e399a14c53ae14ea555f1908157ec8cf483da2
akash <aiazm496@gmail.com>
2022-12-10 13:03:27 +0530

    amazon reviwes

--- src/main/java/com/akash/aws/AmazonReviews.scala
+++ src/main/java/com/akash/aws/AmazonReviews.scala
@@ -3,20 +3,20 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.sql.functions._
 
-//write spark program to process json(2gb), create dataframe with reviewerID, reviewerName, and isSpam
-//load the reviewerID, reviewerName, and isSpam in S3.
-//use Athena to find the top 5 spammers by grouping on reviewerId.
+//write spark program to process json(2gb), create dataframe.
 
-//data is in s3, the spark program will run on EMR and it should put the reviewers data in s3.
 
 object AmazonReviews {
   def main(args: Array[String]): Unit = {
 
     Logger.getLogger("org").setLevel(Level.ERROR)
 
+    //16 core machine so at max 16 tasks can run in parallel.
     val spark = SparkSession.builder().appName("BigLog").master("local[*]").getOrCreate()
 
     var reviewsRawDf = spark.read.format("json").option("path","C:/TrendyTech/bigDataset/Cell_Phones_and_Accessories.json").load
+
+    println(reviewsRawDf.rdd.getNumPartitions)
 
     reviewsRawDf.printSchema()
 //    root
@@ -35,32 +35,32 @@ object AmazonReviews {
 //    |-- summary: string (nullable = true)
 //    |-- unixReviewTime: long (nullable = true)
 
-
     reviewsRawDf.show(false)
     reviewsRawDf = reviewsRawDf.withColumn("order_id", col("_id").getItem("$oid"))
-    reviewsRawDf.show(5,false)
+//    reviewsRawDf.show(5,false)
 
     reviewsRawDf = reviewsRawDf.withColumn("notHelpful",col("helpful")(0)).withColumn("actuallyHelpful",col("helpful")(1))
-    reviewsRawDf.show(5,false)
+//    reviewsRawDf.show(5,false)
 
     reviewsRawDf = reviewsRawDf.withColumnRenamed("class","isSpam")
     reviewsRawDf = reviewsRawDf.drop("_id","asin","helpful","reviewText")
 
     reviewsRawDf.show(1,false)
 
-    reviewsRawDf.printSchema()
-
-//    reviewsRawDf.coalesce(1).write.option("path","C:/TrendyTech/bigDataset/parquetCompressed").option("compression","gzip").mode(SaveMode.Append).save()
-    //copy the gzip to s3 bucket customer-reviews-gzip-parquet
+//    reviewsRawDf.printSchema()
 
     reviewsRawDf.createOrReplaceTempView("reviews")
 
     val topSpammers = spark.sql("select reviewerID,reviewerName,count(order_id) as cnt from reviews where " +
       "isSpam = 1.0 group by reviewerID,reviewerName order by cnt desc limit 5")
+    //shuffling happens 200 tasks are run.
 
-    topSpammers.show()
+    topSpammers.show(5,false)
 
-    topSpammers.coalesce(1).write.format("csv").option("header",true).option("path","C:/TrendyTech/bigDataset").mode(SaveMode.Append).save
+    //can use gzip compression.or write as csv format with bzip2.  by default is snappy for parquet files.
+//    topSpammers.coalesce(1).write
+//      .format("csv").option("header",true).option("path","C:/TrendyTech/bigDataset").mode(SaveMode.Append).save
 
+//    scala.io.StdIn.readLine()
   }
 }
---
+++ src/main/java/com/akash/practice/DataFrame6.scala
@@ -0,0 +1,5 @@
+package com.akash.practice
+
+object DataFrame6 {
+
+}
